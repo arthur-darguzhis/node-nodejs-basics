b8335c96dd311d958c5a59f62836bbccc0283ca0
Arthur Darguzhis <arthur.darguzhis@gmail.com>
2022-12-04 14:28:27 +0300

    implement "zlib:compress"

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,31 @@
-const compress = async () => {
-    // Write your code here 
-};
+import {fileURLToPath} from 'url'
+import zlib from 'node:zlib'
+import stream from 'node:stream'
+import * as fs from 'node:fs'
+import path from 'node:path'
 
-await compress();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const compress = async () => {
+  // Write your code here
+  const sourcePath = __dirname + path.sep + 'files' + path.sep + 'fileToCompress.txt'
+  const destinationPath = __dirname + path.sep + 'files' + path.sep + 'archive.gz'
+
+  const source = fs.createReadStream(sourcePath)
+  const gzip = zlib.createGzip()
+  const destination = fs.createWriteStream(destinationPath)
+
+  stream.pipeline(
+    source,
+    gzip,
+    destination,
+    (err) => {
+      if (err) {
+        console.error('An error occurred:', err)
+        process.exitCode = 1
+      }
+    })
+}
+
+await compress()
