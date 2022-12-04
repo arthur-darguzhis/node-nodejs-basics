f6f8d8a3efdc270c9c6da86d2572775d6bdd1156
Arthur Darguzhis <arthur.darguzhis@gmail.com>
2022-12-04 14:02:59 +0300

    implement "stream:read"

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
-const read = async () => {
-    // Write your code here 
-};
+import * as fs from 'node:fs'
+import path from 'node:path'
+import {fileURLToPath} from 'url'
 
-await read();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const read = async () => {
+  // Write your code here
+  const sourcePath = __dirname + path.sep + 'files' + path.sep + 'fileToRead.txt'
+  const readStream = fs.createReadStream(sourcePath, 'utf-8')
+  readStream.pipe(process.stdout)
+}
+
+await read()
