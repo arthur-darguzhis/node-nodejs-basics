f20da234cbbe8166e6ad2059902af183745da017
Arthur Darguzhis <arthur.darguzhis@gmail.com>
2022-12-04 14:00:01 +0300

    implement "rename"

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
-const rename = async () => {
-    // Write your code here 
-};
+import * as fs from 'node:fs/promises'
+import path from 'node:path'
+import {fileURLToPath} from 'url'
 
-await rename();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const rename = async () => {
+  // Write your code here
+  const oldPath = __dirname + path.sep + 'files/wrongFilename.txt'
+  const newPath = __dirname + path.sep + 'files/properFilename.md'
+
+  try {
+    await fs.rename(oldPath, newPath)
+  } catch (e) {
+    throw new Error('FS operation failed')
+  }
+}
+
+await rename()
