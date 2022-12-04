88335693b45ae91422d3231dfc224104a3c21f76
Arthur Darguzhis <arthur.darguzhis@gmail.com>
2022-12-04 14:00:09 +0300

    implement "delete"

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
-const remove = async () => {
-    // Write your code here 
-};
+import * as fs from 'node:fs/promises'
+import path from 'node:path'
+import {fileURLToPath} from 'url'
 
-await remove();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const remove = async () => {
+  // Write your code here
+  const pathLink = __dirname + path.sep + 'files' +  path.sep + '/fileToRemove.txt'
+
+  try {
+    await fs.unlink(pathLink)
+  } catch (e) {
+    throw new Error('FS operation failed')
+  }
+}
+
+await remove()
