7a93353066ef8f64337bc2e8f00b7e9c9c0a3f3b
Arthur Darguzhis <arthur.darguzhis@gmail.com>
2022-12-04 14:28:50 +0300

    implement "zlib:decompress"

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,31 @@
-const decompress = async () => {
-    // Write your code here 
-};
+import {fileURLToPath} from 'url'
+import zlib from 'node:zlib'
+import stream from 'node:stream'
+import * as fs from 'node:fs'
+import path from 'node:path'
 
-await decompress();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const decompress = async () => {
+  // Write your code here
+  const sourcePath = __dirname + path.sep + 'files' + path.sep + 'archive.gz'
+  const destinationPath = __dirname + path.sep + 'files' + path.sep + 'fileToCompress.txt'
+
+  const source = fs.createReadStream(sourcePath)
+  const gzip = zlib.createUnzip()
+  const destination = fs.createWriteStream(destinationPath)
+
+  stream.pipeline(
+    source,
+    gzip,
+    destination,
+    (err) => {
+      if (err) {
+        console.error('An error occurred:', err)
+        process.exitCode = 1
+      }
+    })
+}
+
+await decompress()
