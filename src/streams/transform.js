d4fa27e57e97de97e47cf656160fc512ea594a5f
Arthur Darguzhis <arthur.darguzhis@gmail.com>
2022-12-04 14:27:57 +0300

    implement "stream:transform"

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,26 @@
-const transform = async () => {
-    // Write your code here 
-};
+import os from 'node:os'
+import stream from 'node:stream'
 
-await transform();
+const transform = async () => {
+  // Write your code here
+  const revert = new stream.Transform({
+    transform(chunk, encoding, callback) {
+      const string = chunk.toString().replace(os.EOL, '')
+      const reverseString = [...string].reverse().join('')
+      callback(null, reverseString + os.EOL)
+    }
+  })
+
+  stream.pipeline(
+    process.stdin,
+    revert,
+    process.stdout,
+    (err) => {
+      err
+        ? console.error('An error occurred in pipeline.', err)
+        : console.log('Pipeline execution successful')
+    }
+  )
+}
+
+await transform()
