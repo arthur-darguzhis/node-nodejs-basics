e99af8e28f64fd0c05fceec7e4ab474a04d76791
Arthur Darguzhis <arthur.darguzhis@gmail.com>
2022-12-04 14:00:39 +0300

    implement "read"

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
-const read = async () => {
-    // Write your code here 
-};
+import * as fs from 'node:fs/promises'
+import path from 'node:path'
+import {fileURLToPath} from 'url'
 
-await read();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const read = async () => {
+  // Write your code here
+  const pathLink = __dirname + path.sep + 'files' + path.sep + 'fileToRead.txt'
+  try {
+    const fileContent = await fs.readFile(pathLink, {encoding: 'utf8'})
+    console.log(fileContent)
+  } catch (e) {
+    throw new Error('FS operation failed')
+  }
+}
+
+await read()
