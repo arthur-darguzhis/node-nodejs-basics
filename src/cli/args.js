7d89bec6f3d40e9284230e974fb01f440993dc21
Arthur Darguzhis <arthur.darguzhis@gmail.com>
2022-12-04 14:01:37 +0300

    implement "args"

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
-};
+  // Write your code here
+  const args = process.argv.reduce((acc, argWithValue) => {
+    if (argWithValue.includes('--')) {
+      const preparedString = argWithValue.
+        replace('--', '').
+        replace('=', ' is ')
 
-parseArgs();
+      acc.push(preparedString)
+    }
+    return acc
+  }, [])
+
+  console.log(args.join(', '))
+}
+
+parseArgs()
